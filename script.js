105cc88197f062ff0656c92498ab933e737fff1b
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-24 19:23:06 +0530

    Add array to store book objects

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+// All book objects are stored in an array
+let myLibrary = [];
+
 function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-    let isReadInWords = isRead ? "read":"not read yet";
-    this.getInfo =  () =>
-        `${this.title} by ${this.author}, ${this.pages} pages, ` + isReadInWords;
+    this.title = title;
+    this.author = author;
+    this.pages = parseInt(pages);
+    this.isRead = isRead;
+    let isReadInWords = this.isRead ? "read":"not read yet";
+    this.getInfo =  () => `${this.title} by ${this.author}, \
+        ${this.pages} pages, ` + isReadInWords;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false)
-console.log(theHobbit.getInfo());
+function addBookToLibrary() {
+    let newBook = getUserInput();
+    const newBookObject = new Book(...newBook);
+    myLibrary.push(newBookObject);
+    return myLibrary;
+}
+
+// Helper function
+function getUserInput() {
+    let bookDetails = [];
+    for (let i = 0; i < 4; i++) {
+        bookDetails.push(window.prompt(`Data ${i + 1}?`))
+    }
+    return bookDetails;
+}
