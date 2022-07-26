d8c61945667607eb308ea9bdb1d496c5b51ce316
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-26 21:37:02 +0530

    Add blur effect and close button for form

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let bookContainer = document.querySelector('.book-container')
+let bookContainer = document.querySelector('.book-container');
+let body = document.querySelectorAll('.body');
+let exit = document.querySelector('.close');
 
 // Event listeners
 document.querySelector(".new-book").addEventListener('click', showForm);
 document.getElementById("popupform").addEventListener('submit', handleForm);
+exit.addEventListener('click', exitForm);
 
 // All book objects are stored in an array
 let myLibrary = [];
@@ -13,14 +16,13 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = parseInt(pages);
     this.isRead = isRead;
-    let isReadInWords = this.isRead == '1'? "read":"not read yet";
+    let isReadInWords = this.isRead === '1'? "read":"not read yet";
     this.getInfo =  () => `${this.title} by ${this.author}, \
         ${this.pages} pages, ` + isReadInWords;
 }
 
 // Adding a book through user input
 function addBookToLibrary(newBook) {
-    // let newBook = getUserInput();
     const newBookObject = new Book(...newBook);
     myLibrary.push(newBookObject);
     displayBook(newBookObject);
@@ -28,22 +30,26 @@ function addBookToLibrary(newBook) {
 
 function showForm() {
     document.getElementById("popupform").classList.remove("hidden");
+    body[0].classList.add("blurred");
+    body[1].classList.add("blurred");
 }
+
 
 // Display a single book as a card
 function displayBook(bookObject) {
     let info = document.createElement("div");
-    info.style.backgroundColor = "rgb(97, 44, 26)";
+    info.style.backgroundColor = "rgba(97, 44, 26, 0.4)";
     info.classList.add("card");
+
     bookContainer.appendChild(info);
     info.innerHTML = bookObject.getInfo();   
+    info.innerHTML = `<p>${bookObject.title}</p> <p>${bookObject.author}</p> <p>${bookObject.pages}</p> <p>${bookObject.isRead === '1'? "Read":"Not read yet"}</p>`;
 }
 
 
 
 function handleForm(event) { 
     event.preventDefault(); 
-    document.getElementById("popupform").classList.add("hidden");
 
     var isRead = document.getElementsByName('read');
     var read_value;
@@ -54,6 +60,16 @@ function handleForm(event) {
     }
     newBookDetails = [title.value, author.value, pages.value, read_value]
     document.getElementById("form").reset();
+    document.getElementById("popupform").classList.add("hidden");
+    body[0].classList.remove("blurred")
+    body[1].classList.remove("blurred")
     return addBookToLibrary(newBookDetails);
 } 
 
+function exitForm(event) {
+    event.preventDefault(); 
+    document.getElementById("form").reset();
+    document.getElementById("popupform").classList.add("hidden");
+    body[0].classList.remove("blurred")
+    body[1].classList.remove("blurred")
+}
