29db085e98ffeb6ecaab354e0523ed2f9063d49c
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-26 09:35:32 +0530

    Add functions to display book details on screen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let bookContainer = document.querySelector('.book-container')
+
 // All book objects are stored in an array
 let myLibrary = [];
 
+// Object constructor
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -11,11 +14,12 @@ function Book(title, author, pages, isRead) {
         ${this.pages} pages, ` + isReadInWords;
 }
 
+// Adding a book through user input
 function addBookToLibrary() {
     let newBook = getUserInput();
     const newBookObject = new Book(...newBook);
     myLibrary.push(newBookObject);
-    return myLibrary;
+    displayBook(newBookObject);
 }
 
 // Helper function
@@ -26,3 +30,13 @@ function getUserInput() {
     }
     return bookDetails;
 }
+
+
+function displayBook(bookObject) {
+    console.log(bookObject.getInfo());
+    let info = document.createElement("div");
+    info.style.backgroundColor = "rgb(255, 0, 0)";
+    bookContainer.appendChild(info);
+    info.innerHTML = bookObject.getInfo();   
+}
+
