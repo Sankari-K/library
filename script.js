72dff7047135fc200e6bfb09ce4dc1fe992fd84f
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-26 15:14:40 +0530

    Use form to update DOM

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let bookContainer = document.querySelector('.book-container')
-let clickNewBook = document.querySelector(".new-book")
+
+// Event listeners
+document.querySelector(".new-book").addEventListener('click', showForm);
+document.getElementById("popupform").addEventListener('submit', handleForm);
 
 // All book objects are stored in an array
 let myLibrary = [];
@@ -10,41 +13,47 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = parseInt(pages);
     this.isRead = isRead;
-    let isReadInWords = this.isRead ? "read":"not read yet";
+    let isReadInWords = this.isRead == '1'? "read":"not read yet";
     this.getInfo =  () => `${this.title} by ${this.author}, \
         ${this.pages} pages, ` + isReadInWords;
 }
 
 // Adding a book through user input
-function addBookToLibrary() {
-    let newBook = getUserInput();
+function addBookToLibrary(newBook) {
+    // let newBook = getUserInput();
     const newBookObject = new Book(...newBook);
     myLibrary.push(newBookObject);
     displayBook(newBookObject);
 }
 
-// Helper function
-function getUserInput() {
-    console.log("hehre");
-    showpopup();
-    let bookDetails = [];
-    // for (let i = 0; i < 4; i++) {
-    //     bookDetails.push(window.prompt(`Data ${i + 1}?`))
-    // }
-    return bookDetails;
+function showForm() {
+    document.getElementById("popupform").classList.remove("hidden");
 }
 
-
+// Display a single book as a card
 function displayBook(bookObject) {
-    console.log(bookObject.getInfo());
     let info = document.createElement("div");
     info.style.backgroundColor = "rgb(97, 44, 26)";
+    info.classList.add("card");
     bookContainer.appendChild(info);
     info.innerHTML = bookObject.getInfo();   
 }
 
-document.querySelector(".new-book").addEventListener('click', addBookToLibrary);
 
-function showpopup() {
-    document.getElementById("popupform").classList.remove("hidden");
-}
+
+function handleForm(event) { 
+    event.preventDefault(); 
+    document.getElementById("popupform").classList.add("hidden");
+
+    var isRead = document.getElementsByName('read');
+    var read_value;
+    for(var i = 0; i < isRead.length; i++){
+        if(isRead[i].checked){
+            read_value = isRead[i].value;
+        }
+    }
+    newBookDetails = [title.value, author.value, pages.value, read_value]
+    document.getElementById("form").reset();
+    return addBookToLibrary(newBookDetails);
+} 
+
